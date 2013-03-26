bb64dbe63fc0f27f15ade9bcdc479d841997fd60
0xD34D <clark@scheffsblend.com>
2013-03-26 15:27:45 -0700

    Added a "Share ChameleonOS" preference to "About Phone"
    Change-Id: I94f77fc518f460d4da28eaaaf100380aab6a50a2

--- src/com/android/settings/DeviceInfoSettings.java
+++ src/com/android/settings/DeviceInfoSettings.java
@@ -38,6 +38,8 @@ import java.io.IOException;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
+import com.android.settings.R;
+
 public class DeviceInfoSettings extends SettingsPreferenceFragment {
 
     private static final String LOG_TAG = "DeviceInfoSettings";
@@ -71,6 +73,7 @@ public class DeviceInfoSettings extends SettingsPreferenceFragment {
     private static final String KEY_DEVICE_CPU = "device_cpu";
     private static final String KEY_DEVICE_MEMORY = "device_memory";
     private static final String KEY_COS_UPDATES = "cos_updates";
+    private static final String KEY_COS_SHARE = "share";
 
     static final int TAPS_TO_BE_A_DEVELOPER = 7;
     long[] mHits = new long[3];
@@ -253,6 +256,13 @@ public class DeviceInfoSettings extends SettingsPreferenceFragment {
                     Log.e(LOG_TAG, "Unable to start activity " + intent.toString());
                 }
             }
+        } else if (preference.getKey().equals(KEY_COS_SHARE)) {
+		    Intent intent = new Intent();
+		    intent.setAction(Intent.ACTION_SEND);
+		    intent.setType("text/plain");
+		    intent.putExtra(Intent.EXTRA_TEXT, String.format(
+                getActivity().getString(R.string.share_message), Build.MODEL));
+		    startActivity(Intent.createChooser(intent, getActivity().getString(R.string.share_chooser_title)));
         }
         return super.onPreferenceTreeClick(preferenceScreen, preference);
     }
