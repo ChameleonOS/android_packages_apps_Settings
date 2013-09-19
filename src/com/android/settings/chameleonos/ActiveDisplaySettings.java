8ab78d4d79bf124bda5b78dc2bdf9e44df25c2e2
0xD34D <clark@scheffsblend.com>
2013-09-18 19:23:53 -0700

    Active display: Add options for date and am/pm [2/2]
    Change-Id: I6af47b2fd47e77398eee98cc6e30b8fcf1a44d45

--- src/com/android/settings/chameleonos/ActiveDisplaySettings.java
+++ src/com/android/settings/chameleonos/ActiveDisplaySettings.java
@@ -33,17 +33,21 @@ import static android.hardware.Sensor.TYPE_PROXIMITY;
 
 public class ActiveDisplaySettings extends SettingsPreferenceFragment implements
         OnPreferenceChangeListener {
-    private static final String TAG = "PornSettings";
+    private static final String TAG = "ActiveDisplaySettings";
 
     private static final String KEY_ENABLED = "ad_enable";
     private static final String KEY_SHOW_TEXT = "ad_text";
     private static final String KEY_ALL_NOTIFICATIONS = "ad_all_notifications";
     private static final String KEY_POCKET_MODE = "ad_pocket_mode";
     private static final String KEY_REDISPLAY = "ad_redisplay";
+    private static final String KEY_SHOW_DATE = "ad_show_date";
+    private static final String KEY_SHOW_AMPM = "ad_show_ampm";
     private static final String KEY_BRIGHTNESS = "ad_brightness";
 
     private SwitchPreference mEnabledPref;
     private CheckBoxPreference mShowTextPref;
+    private CheckBoxPreference mShowDatePref;
+    private CheckBoxPreference mShowAmPmPref;
     private CheckBoxPreference mAllNotificationsPref;
     private CheckBoxPreference mPocketModePref;
     private ListPreference mRedisplayPref;
@@ -82,6 +86,14 @@ public class ActiveDisplaySettings extends SettingsPreferenceFragment implements
                 Settings.System.ACTIVE_DISPLAY_REDISPLAY, 0);
         mRedisplayPref.setValue(String.valueOf(timeout));
         updateRedisplaySummary(timeout);
+
+        mShowDatePref = (CheckBoxPreference) findPreference(KEY_SHOW_DATE);
+        mShowDatePref.setChecked((Settings.System.getInt(getContentResolver(),
+                Settings.System.ACTIVE_DISPLAY_SHOW_DATE, 0) == 1));
+
+        mShowAmPmPref = (CheckBoxPreference) findPreference(KEY_SHOW_AMPM);
+        mShowAmPmPref.setChecked((Settings.System.getInt(getContentResolver(),
+                Settings.System.ACTIVE_DISPLAY_SHOW_AMPM, 0) == 1));
 
         mBrightnessLevel = (SeekBarPreference) findPreference(KEY_BRIGHTNESS);
         mBrightnessLevel.setValue(Settings.System.getInt(getContentResolver(),
@@ -126,6 +138,16 @@ public class ActiveDisplaySettings extends SettingsPreferenceFragment implements
             value = mPocketModePref.isChecked();
             Settings.System.putInt(getContentResolver(),
                     Settings.System.ACTIVE_DISPLAY_POCKET_MODE,
+                    value ? 1 : 0);
+        } else if (preference == mShowDatePref) {
+            value = mShowDatePref.isChecked();
+            Settings.System.putInt(getContentResolver(),
+                    Settings.System.ACTIVE_DISPLAY_SHOW_DATE,
+                    value ? 1 : 0);
+        } else if (preference == mShowAmPmPref) {
+            value = mShowAmPmPref.isChecked();
+            Settings.System.putInt(getContentResolver(),
+                    Settings.System.ACTIVE_DISPLAY_SHOW_AMPM,
                     value ? 1 : 0);
         } else {
             return super.onPreferenceTreeClick(preferenceScreen, preference);
